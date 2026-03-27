b6b6ef80e0e9e43f5e1c689ccefd3e3934ebede7
Karam Akil <karamgurme2020gurmesi@gmail.com>
2026-03-27 19:46:11 +0300

    Update led.c

--- Led/led_timer_ctc_interrupt/led.c
+++ Led/led_timer_ctc_interrupt/led.c
@@ -7,13 +7,7 @@
 #include <avr/interrupt.h>
 // This program toggles the pin PB5 every 1 second using the timer0 on the atmega328p
 typedef unsigned char byte;
-#define DDRB *((volatile byte*)0x24);
-#define PINB *((volatile byte*)0x23);
-#define PB5 5
-#define WGM01 1
-#define CS00 0
-#define CS02 2
-#define OCIE0A 1
+
 
 
 int main(void){
